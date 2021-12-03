6beb33fd8f51ddcb41453565f63d1c4cf5aab0c7
nro04 <ronicolas04@gmail.com>
2021-12-03 09:39:35 -0500

    Set Up laravel

---
+++ database/migrations/2021_12_03_142534_tb_users_ospedale.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TbUsersOspedale extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('nit');
+            $table->string('password');
+            $table->string('gender');
+            $table->string('date_birth');
+            $table->string('phone');
+            $table->foreignId('rol_id')->references('id')->on('tb_roles');
+            $table->foreignId('eps_id')->references('id')->on('tb_eps');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_users');
+    }
+}
